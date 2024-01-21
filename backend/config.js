d220ad56692815826c8fc377f9c6294ee72306f8
anshul-999 <anshul.b100@gmail.com>
2024-01-21 18:22:27 +0530

    Modified Gitignore

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT= 5555;
 
 export const mongoDBURL =
-"mongodb+srv://anshulb100:book@cluster0.rrpznqs.mongodb.net/?retryWrites=true&w=majority"
+"mongodb+srv://anshulb100:bookstore@cluster0.mp5wpek.mongodb.net/?retryWrites=true&w=majority"
